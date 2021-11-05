0897927a7d04666bbee6a6b06e5de9e0aeb9e8a8
Roshni <sparksroshni610@gmail.com>
2021-11-06 02:06:46 +0530

    darkthemeadded2

--- src/components/Homepage/ExpenseWrapper.jsx
+++ src/components/Homepage/ExpenseWrapper.jsx
@@ -1,9 +1,8 @@
 import React, { Fragment, useContext } from "react";
-import { GlobalContext } from "../../context/ExpenseContext";
+
 import ExpenseItemList from "./ExpenseItemList";
 
 const ExpenseWrapper = () => {
-  let { theme } = useContext(GlobalContext);
   return (
     <div className="ExpenseWrapper">
       <div className={`container expense-wrapper-heading `}>
